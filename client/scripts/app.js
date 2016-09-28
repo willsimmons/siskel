2c7f5ecbd635d94eed3b40934178ec595af76843
willsimmons <willsimmons@gmail.com>
2016-09-28 14:57:33 -0700

    complete
    all tests complete

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -15,18 +15,26 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', function() {
+      this.sortByField(this.comparator);
+    });
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field; 
+    return this.sort(field);
   }
 
 });
 
 var AppView = Backbone.View.extend({
+  initialize: function() {
+    this.collection.on('sort', function() {
+      this.render();
+    }, this);
+  },
 
   events: {
     'click form input': 'handleClick'
@@ -58,7 +66,8 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', this.render, this);
+    this.render();
   },
 
   events: {
@@ -66,7 +75,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +88,8 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.render();
+    this.collection.on('change', this.render, this);
   },
 
   render: function() {
